e099bd15d1a962f5b3120d721b691f5bfb2a398d
Patrick Robertson <Patrick@patricks-macbook-pro.local>
2016-12-05 11:54:26 +1100

    Initial Commit

---
+++ TipsyTipCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TipsyTipCalculator
+//
+//  Created by Patrick Robertson on 5/12/2016.
+//  Copyright © 2016 Patrick Robertson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
